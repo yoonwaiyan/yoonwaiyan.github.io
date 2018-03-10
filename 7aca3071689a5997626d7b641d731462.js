33e7f5eecfb679295112bfe355819e2a30fca2fc
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-03-10 17:36:43 +0800

    use Parcel to build pages

---
+++ 7aca3071689a5997626d7b641d731462.js
@@ -0,0 +1,5 @@
+require=function(r,e,n){function t(n,o){function i(r){return t(i.resolve(r))}function f(e){return r[n][1][e]||e}if(!e[n]){if(!r[n]){var c="function"==typeof require&&require;if(!o&&c)return c(n,!0);if(u)return u(n,!0);var l=new Error("Cannot find module '"+n+"'");throw l.code="MODULE_NOT_FOUND",l}i.resolve=f;var s=e[n]=new t.Module(n);r[n][0].call(s.exports,i,s,s.exports)}return e[n].exports}function o(r){this.id=r,this.bundle=t,this.exports={}}var u="function"==typeof require&&require;t.isParcelRequire=!0,t.Module=o,t.modules=r,t.cache=e,t.parent=u;for(var i=0;i<n.length;i++)t(n[i]);return t}({7:[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=function(){console.log("Hello.")};
+},{}],5:[function(require,module,exports) {
+"use strict";var e=require("./main"),u=r(e);function r(e){return e&&e.__esModule?e:{default:e}}(0,u.default)();
+},{"./main":7}]},{},[5])
