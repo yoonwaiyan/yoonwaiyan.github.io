66d710119da9561d99bc81e8eb1fcf7532b7ae70
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-11-05 19:37:11 +0800

    Updates

---
+++ precache-manifest.9f386c28c0775aaf5ba43eecc587dcb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8fd3fddb6ffecdd4bb4",
+    "url": "/static/js/main.a8fd3fdd.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "a8fd3fddb6ffecdd4bb4",
+    "url": "/static/css/main.a3ab4d68.chunk.css"
+  },
+  {
+    "revision": "beae3ddfaaa45a3c7f81beb693fe09a4",
+    "url": "/index.html"
+  }
+];
