3c87a9d88fc150989db633e926d3c1dc3f55d073
Yoon Wai Yan <waiyanyoon@gmail.com>
2018-11-05 19:34:40 +0800

    revamp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>waiyan.yoon</h1>
+        <div>coming.soon</div>
+      </div>
+    );
+  }
+}
+
+export default App;
