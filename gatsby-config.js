4537e7c518c263a4b86e09ac284012d7761df761
Yoon Wai Yan <waiyanyoon@gmail.com>
2021-06-12 23:28:59 +0800

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,13 +82,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `waiyan.yoon`,
+        short_name: `waiyan.yoon`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
