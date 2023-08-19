d00b191b383cc98666a411707ee00958bd493180
Yoon Wai Yan <waiyanyoon@gmail.com>
2023-08-19 20:24:56 +0800

    Upgrade to Gatsby v5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  purge: ['./src/{pages,components}/*.{js,jsx,ts,tsx}'],
+  content: ['./src/{pages,components}/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      borderWidth: ['first'],
-    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
